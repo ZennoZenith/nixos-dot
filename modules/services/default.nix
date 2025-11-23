b8557ac27a42ba1604cb20fa1c33a858f2c4def9
Zenno Zenith <95485961+ZennoZenith@users.noreply.github.com>
2025-11-23 17:03:42 +0530

    Generation 063

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -8,6 +8,10 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+    dataDir = "/home/knack";
+    # configDir = "/home/knack/.config/syncthing";
+    user = "knack";
+    group = "users";
   };
   ## port 8384  is the default port to allow access from the network.
   # networking.firewall.allowedTCPPorts = [ 8384 ];
