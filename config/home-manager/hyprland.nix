87f320c9ad97b71ad0267ed355c731d42756005b
Know Knack <201266042+knowknack@users.noreply.github.com>
2025-12-20 10:33:32 +0530

    System upgrade

--- config/home-manager/hyprland.nix
+++ config/home-manager/hyprland.nix
@@ -12,7 +12,6 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
 
     plugins = [
-      ## DISABLED due to some incompability
       # inputs.hypr-dynamic-cursors.packages.${pkgs.stdenv.hostPlatform.system}.hypr-dynamic-cursors
       pkgs.hyprcursor
       inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo
@@ -502,24 +501,21 @@
       plugin = {
         dynamic-cursors = {
           # enables the plugin
-          enabled = true;
+          ## FIX: DISABLED due to some incompability
+          enabled = false;
 
-          # sets the cursor behaviour, supports these values:
-          # tilt    - tilt the cursor based on x-velocity
-          # rotate  - rotate the cursor based on movement direction
-          # stretch - stretch the cursor shape based on direction and velocity
-          # none    - do not change the cursors behaviour
-          mode = "tilt";
+          # sets the cursor behaviour
+          mode = "stretch"; # tilt, rotate, stretch, none
 
           # minimum angle difference in degrees after which the shape is changed
           # smaller values are smoother, but more expensive for hw cursors
-          threshold = 4;
+          threshold = 2;
 
           # for mode = rotate
           rotate = {
             # length in px of the simulated stick used to rotate the cursor
             # most realistic if this is your actual cursor size
-            length = 20;
+            length = 24;
 
             # clockwise offset applied to the angle in degrees
             # this will apply to ALL shapes
@@ -531,7 +527,7 @@
             # controls how powerful the tilt is, the lower, the more power
             # this value controls at which speed (px/s) the full tilt is reached
             # the full tilt being 60° in both directions
-            limit = 3000;
+            limit = 2000;
 
             # relationship between speed and tilt, supports these values:
             # linear             - a linear function is used
@@ -542,26 +538,70 @@
 
             # time window (ms) over which the speed is calculated
             # higher values will make slow motions smoother but more delayed
-            window = 100;
+            window = 50;
           };
         };
 
-        # use hyprcursor to get a higher resolution texture when the cursor is magnified
-        # see the `hyprcursor` section below
-        hyprcursor = {
-          # use nearest-neighbour (pixelated) scaling when magnifing beyond texture size
-          # this will also have effect without hyprcursor support being enabled
-          # 0 / false - never use pixelated scaling
-          # 1 / true  - use pixelated when no highres image
-          # 2         - always use pixleated scaling
+        # for mode = stretch
+        stretch = {
+          # controls how much the cursor is stretched
+          # this value controls at which speed (px/s) the full stretch is reached
+          # the full stretch being twice the original length
+          limit = 2000;
+
+          # relationship between speed and stretch amount, supports these values:
+          # linear             - a linear function is used
+          # quadratic          - a quadratic function is used
+          # negative_quadratic - negative version of the quadratic one, feels more aggressive
+          # see `activation` in `src/mode/utils.cpp` for how exactly the calculation is done
+          function = "quadratic";
+
+          # time window (ms) over which the speed is calculated
+          # higher values will make slow motions smoother but more delayed
+          window = 50;
+        };
+
+        # configure shake to find magnifies the cursor if its is being shaken
+        shake = {
+          enabled = true;
+
+          # use nearest-neighbour (pixelated) scaling when shaking
           nearest = true;
 
+          # controls how soon a shake is detected
+          threshold = 3.0;
+
+          # magnification level immediately after shake start
+          base = 4.0;
+
+          # magnification increase per second when continuing to shake
+          speed = 8.0;
+
+          # how much the speed is influenced by the current shake intensitiy
+          influence = 0.0;
+
+          # maximal magnification the cursor can reach
+          limit = 0.0; # values below 1 disable the limit (e.g. 0)
+
+          # time in millseconds the cursor will stay magnified after a shake has ended
+          timeout = 3000;
+
+          # show cursor behaviour `tilt`, `rotate`, etc. while shaking
+          effects = true;
+
+          # enable ipc events for shake
+          ipc = true;
+        };
+
+        # use hyprcursor to get a higher resolution texture when the cursor is magnified
+        hyprcursor = {
           # enable dedicated hyprcursor support
           enabled = true;
 
+          # use nearest-neighbour (pixelated) scaling when magnifing beyond texture size
+          nearest = true;
+
           # resolution in pixels to load the magnified shapes at
-          # be warned that loading a very high-resolution image will take a long time and might impact memory consumption
-          # -1 means we use [normal cursor size] * [shake:base option]
           resolution = -1;
 
           # shape to use when clientside cursors are being magnified
